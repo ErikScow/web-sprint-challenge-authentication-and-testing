00a683a60c81afc3421a0ed2f7db374b2256461a
Erik Scow <erikscow@gmail.com>
2020-08-14 18:28:12 -0700

    added register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const db = require('../database/dbConfig')
+
 router.post('/register', (req, res) => {
-  // implement registration
+  const credentials = req.body
+  const hash = bcrypt.hashSync(credentials.password, 12)
+  credentials.password = hash
+  db('users').insert(req.body)
+      .then(()=>{
+          res.status(201).json({message: "user successfully created"})
+      })
+      .catch(err => {
+          console.log(err)
+          res.status(500).json({message: "could not add to database"})
+      })
 });
 
 router.post('/login', (req, res) => {
