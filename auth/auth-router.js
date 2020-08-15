afcd0b0b39843486e1b7a4078bf3128d2b23f1cd
Erik Scow <erikscow@gmail.com>
2020-08-14 18:31:26 -0700

    added login endpoint using jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const db = require('../database/dbConfig')
+const secrets = require('../secrets')
 
 router.post('/register', (req, res) => {
   const credentials = req.body
@@ -19,7 +20,38 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body
+
+  db('users').where('username', username)
+      .first()
+      .then(user =>{
+          if (user && bcrypt.compareSync(password, user.password)) {
+              const token = generateToken(user)
+              res.status(200).json({
+                  message: "successful login",
+                  token
+              })
+          } else {
+              res.status(401).json({ message: "invalid credentials"})
+          }
+      })
+      .catch(err => {
+          console.log(err)
+          res.status(500).json({ message: "database error"})
+      })
 });
+
+function generateToken(user){
+  const payload = {
+      subject: user.id,
+      username: user.username
+  }
+
+  const options = {
+      expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
